b5c2e12f589747d629630ad6a0278a6907e1390f
Viet My <Ziettmy@gmail.com>
2024-04-17 00:01:19 +0700

    doneex4.js

--- C1-S3-PRACTICE/ex4.js
+++ C1-S3-PRACTICE/ex4.js
@@ -3,18 +3,24 @@ let numbers = [1, 2, 3, 4, 5];
 // Write the function to filter out even numbers from an array
 // You need to use the array.filter() function
 function filterEvenNumbers(arr) {
+  let evenNumbersArr= arr.filter((s)=>s%2==0)
+  return evenNumbersArr;
   // TODO - Your code here (1 line)
 }
 
 // Function to map array elements to their squared values
 // You need to use the array.map() function
 function squareNumbers(arr) {
+  let squaredArr= arr.map((s)=>s*s);
+  return squaredArr;
   //  TODO - Your code here (1 line)
 }
 
 // Function to compute the sum of array elements
 // You need to use the array.reduce() function
 function sumArray(arr) {
+  let sum = arr.reduce((total, currentValue)=> total+currentValue) ;
+  return (sum);
   //  TODO - Your code here (1 line)
 }
 
