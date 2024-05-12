8df047db5f05b1222fb36643da636e77daf348a2
Viet My <Ziettmy@gmail.com>
2024-05-12 17:02:26 +0700

    done ex2

--- S3-JSX/exercise-2/src/App.jsx
+++ S3-JSX/exercise-2/src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Header from "./Header.jsx";
+import Footer  from "./Footer.jsx";
+import Body from "./Body.jsx";
 function App() {
   return (
     <>
-      <p>Hello</p>
+      <Header/>
+      <Body/>
+      <Footer/>
     </>
   );
 }
+
 export default App;
